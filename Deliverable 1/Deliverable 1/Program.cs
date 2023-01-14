290533903c88bea60caf0fb08acc63d7c3e3ac5b
jackievoh <72594447+jackievoh@users.noreply.github.com>
2023-01-14 06:15:32 -0500

    Add files via upload

---
+++ Deliverable 1/Deliverable 1/Program.cs
@@ -0,0 +1,8 @@
+﻿// This was done on the Mac version of Visual Studio 2022
+
+// Solution 1
+Console.WriteLine("Hello, World!");
+
+// Solution 2
+// string hello = "Hello, World!";
+// Console.WriteLine(hello);
